3babf0b5a19f98ac802a9bca9ba3ee5718508dfb
Mariusz Matlak <1mariusz.matlak@gmail.com>
2019-04-20 15:58:12 +0200

    add const variables in program

---
+++ main.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const taskNumber = document.querySelector('h1 span');
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector('input');
