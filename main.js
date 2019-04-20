9bd43e02daf677730c151811d0285e88b5615c59
Mariusz Matlak <1mariusz.matlak@gmail.com>
2019-04-20 16:04:51 +0200

    add function

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h1 span');
 const listItems = document.getElementsByClassName('task');
 const input = document.querySelector('input');
+
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    taskNumber.textContent = listItems.length;
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if (titleTask === '') return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + " <button>Delete task</button>"
+    ul.appendChild(task);
+    input.value = '';
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
+}
+
+form.addEventListener('submit', addTask);
